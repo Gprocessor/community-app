2a26a0f1e98f9be60849494104476db46d3a9143
Leonardo Hildt <leonardo@fiter.io>
2023-09-09 16:24:55 -0300

    TSS-105 Limit login re-tries
    Removed parameter

--- app/scripts/services/ResourceFactoryProvider.js
+++ app/scripts/services/ResourceFactoryProvider.js
@@ -264,7 +264,7 @@
                     userListResource: defineResource(apiVer + "/users/:userId", {userId: '@userId'}, {
                         getAllUsers: {method: 'GET', params: {}, isArray: true},
                         update: { method: 'PUT' },
-                        blockOrUnblock: {method: 'POST', params: {userId: '@userId',command: '@command', blockDays: '@blockDays'}}
+                        blockOrUnblock: {method: 'POST', params: {userId: '@userId',command: '@command'}}
                     }),
                     userTemplateResource: defineResource(apiVer + "/users/template", {}, {
                         get: {method: 'GET', params: {}}
