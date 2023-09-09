7fe4bc05071392c135004b1648959fd6013ec09c
Leonardo Hildt <leonardo@fiter.io>
2023-09-09 16:20:07 -0300

    Feature TSS-105 Limit login re-tries

--- app/scripts/services/ResourceFactoryProvider.js
+++ app/scripts/services/ResourceFactoryProvider.js
@@ -263,7 +263,8 @@
                     }),
                     userListResource: defineResource(apiVer + "/users/:userId", {userId: '@userId'}, {
                         getAllUsers: {method: 'GET', params: {}, isArray: true},
-                        update: { method: 'PUT' }
+                        update: { method: 'PUT' },
+                        blockOrUnblock: {method: 'POST', params: {userId: '@userId',command: '@command', blockDays: '@blockDays'}}
                     }),
                     userTemplateResource: defineResource(apiVer + "/users/template", {}, {
                         get: {method: 'GET', params: {}}
